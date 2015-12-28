cea7e50af271b5fac6c472815a5cf84e563a183e
David Charte <fdavidcl@outlook.com>
2015-12-28 21:20:24 +0100

    Añade controlador vacío

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :medios
+  resources :espacios
 
   # Example resource route with options:
   #   resources :products do
