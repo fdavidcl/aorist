3c5ea0bc8284e8cc7c6efb51166349e7da7cb962
JCEntrena <jentrena@correo.ugr.es>
2015-12-30 19:58:53 +0200

    Relaciones y validates de los Contratos a Medios. Importe > 0 y creación del contrato antes de finalización.

--- app/app/models/contrato_medio.rb
+++ app/app/models/contrato_medio.rb
@@ -1,2 +1,7 @@
 class ContratoMedio < ActiveRecord::Base
+  has_many :pagos
+  belongs_to :medio
+  validates :importe, presence: true, :numericality => { :greather_than => 0 }  # Comprobar que esto sea correcto. 
+  validates_date_of :duracion, after: :fecha
+  
 end
