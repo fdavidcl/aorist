1d721098d69df7ec4f2e4bfc08ae19f1ddd66e6f
JCEntrena <jentrena@correo.ugr.es>
2015-12-30 19:43:22 +0200

    Relación y validates de pago.

--- app/app/models/pago.rb
+++ app/app/models/pago.rb
@@ -1,2 +1,6 @@
 class Pago < ActiveRecord::Base
+  belongs_to :contrato_medio
+  validates :importe, presence: true, :numericality => { :greather_than => 0 }  # Comprobar que esto sea correcto. 
+  
 end
+
