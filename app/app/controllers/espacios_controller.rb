7c78bcb292f1cb1dfc39a0b2c9e79cdef58ee08b
David Charte <fdavidcl@outlook.com>
2015-12-30 02:11:25 +0100

    Avances en espacios

--- app/app/controllers/espacios_controller.rb
+++ app/app/controllers/espacios_controller.rb
@@ -1,6 +1,25 @@
 class EspaciosController < ApplicationController
+  private
+  def espacio_params
+    params.require(:espacio).permit(:multimedia, :enlace, :ancho, :alto)
+  end
+
+  public
   def index
     @medio = Medio.find_by :id => params[:medio_id]
     @espacios = Espacio.where :medio => params[:medio_id]
+  end
+
+  def new
+    @medio = Medio.find_by :id => params[:medio_id]
+    @espacio = Espacio.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @espacio = Espacio.new espacio_params
   end
 end
