a9cc2d6d74c47776386016d95928a78b555d2fc1
David Charte <fdavidcl@outlook.com>
2016-01-01 15:21:33 +0100

    Fix typo with Hash notation
    key: value, not key :value

--- app/app/models/medio.rb
+++ app/app/models/medio.rb
@@ -1,8 +1,8 @@
 class Medio < ActiveRecord::Base
-  has_many :contrato_medios 
-  has_many :espacios 
+  has_many :contrato_medios
+  has_many :espacios
   has_many :audiences
-  validates :nombre, :web, :fiscales, presence :true, :length => {:minimum => 1}
+  validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
 
   def espacios
     Espacio.where :medio_id => self.id
