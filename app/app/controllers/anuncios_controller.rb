d8158097b9f783fd8587cfe2429e3900d00db389
JCEntrena <jentrena@correo.ugr.es>
2016-01-02 23:51:07 +0200

    Controlador para anuncios. Falta método show.

---
+++ app/app/controllers/anuncios_controller.rb
@@ -0,0 +1,34 @@
+class AnunciosController < ApplicationController
+  
+  private
+  def anuncio_params
+    params.require(:anuncio).permit(:contenido, :URL)
+  end
+
+  public
+  def index
+    @anunciante = Anunciante.find_by :id => params[:anunciante_id]
+    @anuncios = Anuncio.where :anunciante => params[:anunciante_id]
+  end
+  
+  def new
+    @anunciante = Anunciante.find_by :id => params[:anunciante_id]
+    @anuncio = Anuncio.new
+  end
+  
+  def show
+  
+  end
+  
+  def create
+    @anuncio = Anuncio.new anuncio_params
+  end
+  
+  def destroy 
+    @anuncio = Anuncio.find params[:id]
+    @anuncio.destroy
+    
+    redirect_to anuncios_path
+  end
+  
+end
