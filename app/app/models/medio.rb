7aabba76293c72de2835f3419d7c636c0fc5e07b
JCEntrena <jentrena@correo.ugr.es>
2016-01-02 21:54:40 +0200

    Commit para bajar cambios.

--- app/app/models/medio.rb
+++ app/app/models/medio.rb
@@ -2,7 +2,8 @@ class Medio < ActiveRecord::Base
   has_many :contrato_medios 
   has_many :espacios 
   has_many :audiences
-  validates :nombre, :web, :fiscales, presence :true, :length => {:minimum => 1}
+  validates :nombre, length: { minimum: 1 }
+  validates :web, :fiscales, presence: true
 
   def espacios
     Espacio.where :medio_id => self.id
