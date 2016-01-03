23e3735ceb33ffdda00479cf70076bf3826d644b
JCEntrena <jentrena@correo.ugr.es>
2016-01-03 20:34:56 +0200

    Controlador para audiencias.

--- app/app/controllers/audiences_controller.rb
+++ app/app/controllers/audiences_controller.rb
@@ -1,4 +1,37 @@
 class AudiencesController < ApplicationController
-  def index
+  private
+  def audience_params
+    params.require(:audience).permit(:nombre, :descripcion)
   end
+  
+  public  
+  def new
+    @audience = Audience.new
+  end
+
+  def create
+    @audience = Aucience.new audience_params
+    
+    if @audience.save
+      redirect_to @audience
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @aucience = Audience.find params[:id]
+  end
+
+  def index
+    @audiences = Audience.all
+  end
+  
+  def destroy
+    @audience = Audience.find params[:id]
+    @audience.destroy
+    
+    redirect_to audiences_path  
+  end
+  
 end
