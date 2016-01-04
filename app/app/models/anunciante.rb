c6585f7b43f7b0ebeac348b6f3391af7986ac8a7
David Charte <fdavidcl@outlook.com>
2016-01-04 20:26:34 +0100

    Elimina métodos redundantes en modelos
    Los métodos que se refieren a entidades asociadas a un modelo ya vienen implementados por ActiveRecord

--- app/app/models/anunciante.rb
+++ app/app/models/anunciante.rb
@@ -3,8 +3,4 @@ class Anunciante < ActiveRecord::Base
   has_many :anunciante_contratos
   has_many :audiences
   validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
-
-  def anuncios
-    Anuncio.where anunciante_id: self.id
-  end
 end
--- app/app/models/medio.rb
+++ app/app/models/medio.rb
@@ -3,8 +3,4 @@ class Medio < ActiveRecord::Base
   has_many :espacios
   has_many :audiences
   validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
-
-  def espacios
-    Espacio.where medio_id: self.id
-  end
 end
