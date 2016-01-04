cb9b9f642640c5ff4e80aac0b8de1768c11a4272
JCEntrena <jentrena@correo.ugr.es>
2016-01-05 01:29:15 +0200

    Arreglados confilctos con merge.

--- app/app/controllers/anunciantes_controller.rb
+++ app/app/controllers/anunciantes_controller.rb
@@ -34,18 +34,10 @@ class AnunciantesController < ApplicationController
 
     redirect_to anunciantes_path
   end
-<<<<<<< HEAD
   
-  # Check this out. 
   def asociate_audience
     @audience = Audience.find_by id: params[:audience_id]
     @anunciante = Anunciante.find_by id: params[:id]
-=======
-
-  def associate_audience
-    @audience = Audience.find params[:audience_id]
-    @anunciante = Anunciante.find params[:id]
->>>>>>> f5860d7e555053c8f8badc745d9f796e383091ae
     @anunciante.audiences << @audience
   end
 
--- app/app/controllers/medios_controller.rb
+++ app/app/controllers/medios_controller.rb
@@ -26,33 +26,20 @@ class MediosController < ApplicationController
   def index
     @medios = Medio.all
   end
-<<<<<<< HEAD
+
   
   def destroy 
     @medio = Medio.find_by id: params[:id]
-=======
-
-  def destroy
-    @medio = Medio.find params[:id]
->>>>>>> f5860d7e555053c8f8badc745d9f796e383091ae
     @medio.destroy
 
     redirect_to medios_path
-<<<<<<< HEAD
   end 
   
   def asociate_audience
     @audience = Audience.find_by id: params[:audience_id]
     @medio = Medio.find_by id: params[:id]
     @medio.audiences << @audience  
-=======
   end
 
-  def associate_audience
-    @audience = Audience.find params[:audience_id]
-    @medio = Medio.find params[:id]
-    @medio.audiences << @audience
->>>>>>> f5860d7e555053c8f8badc745d9f796e383091ae
-  end
 
 end
