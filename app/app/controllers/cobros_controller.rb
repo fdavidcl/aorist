ac557d2f6f212c7df5b44e6f5454c9d6e78cf59b
JCEntrena <jentrena@correo.ugr.es>
2016-01-05 02:10:22 +0200

    Pequeños cambios.

--- app/app/controllers/cobros_controller.rb
+++ app/app/controllers/cobros_controller.rb
@@ -1,4 +1,32 @@
 class CobrosController < ApplicationController
-  def index
+
+  private
+  def cobro_params
+  
   end
+  
+  public
+  def new
+    @cobro = Cobro.new
+  end
+  
+  def create
+  
+  end
+  
+  def show
+  
+  end 
+
+  def index
+  
+  end
+  
+  def destroy 
+    @cobro = Cobro.find_by id: params[:id]
+    @cobro.destroy
+    
+    redirect_to cobros_path       # Puede que haya que venir a través de anunciantes. 
+  end 
+ 
 end
--- app/app/controllers/pagos_controller.rb
+++ app/app/controllers/pagos_controller.rb
@@ -1,4 +1,31 @@
 class PagosController < ApplicationController
-  def index
+
+  private
+  def pago_params
+  
   end
+  
+  public
+  def new
+    @pago = Pago.new
+  end
+  
+  def create
+    @pago = Pago.new pago_params
+  end
+  
+  def show
+  
+  end 
+
+  def index
+  
+  end
+  
+  def destroy 
+    @pago = Pago.find_by id: params[:id]
+    @pago.destroy
+    
+    redirect_to pagos_path          # Puede que haya que venir a través de medios. 
+  end 
 end
