b6f1fe807893f32dca4a7f62bdb4dfeb1cd6605f
JCEntrena <jentrena@correo.ugr.es>
2016-01-06 22:49:42 +0200

    Implementando.

--- app/app/controllers/anunciante_contratos_controller.rb
+++ app/app/controllers/anunciante_contratos_controller.rb
@@ -1,4 +1,35 @@
 class AnuncianteContratosController < ApplicationController
-  def index
+  private
+  def anunciante_contrato_params
+  
   end
+  
+  public   
+  def new
+    @contrato = AnuncianteContrato.new
+  end 
+  
+  def create
+    @contrato = AnuncianteContrato.new anunciante_contrato_params
+    
+    if @contrato.save 
+      redirect_to @contrato
+    else
+      render :new
+    end
+  
+  end
+  
+  def show
+    @contrato = AnuncianteContrato.find_by id: params[:id]
+  end
+
+  def index
+    @contratos = AnuncianteContrato.all
+  end
+  
+  def destroy
+  
+  end
+  
 end
--- app/app/controllers/medio_contratos_controller.rb
+++ app/app/controllers/medio_contratos_controller.rb
@@ -1,4 +1,35 @@
 class MedioContratosController < ApplicationController
-  def index
+  private
+  def medio_contrato_params
+  
   end
+  
+  public
+  def new
+    @contrato = MedioContrato.new
+  end 
+  
+  def create
+    @contrato = MedioContrato.new medio_contrato_params
+    
+    if @contrato.save 
+      redirect_to @contrato
+    else
+      render :new
+    end
+  
+  end
+  
+  def show
+    @contrato = MedioContrato.find_by id: params[:id]
+  end
+
+  def index
+    @contratos = MedioContrato.all
+  end
+  
+  def destroy
+  
+  end
+  
 end
