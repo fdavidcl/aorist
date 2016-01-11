5f0eb3bc241d107615cf2af767bc26c94eada63d
David Charte <fdavidcl@outlook.com>
2016-01-11 13:11:46 +0100

    Añade ruta para contabilidad#show

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -24,8 +24,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'contabilidad' => 'contabilidad#show'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
