acc76f08061afc89cd30b344b9157adbb1cd8019
JCEntrena <jentrena@correo.ugr.es>
2016-01-18 15:15:08 +0200

    Arreglados fallos.

--- app/instala_ruby-oci8.sh
+++ app/instala_ruby-oci8.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo echo "
+sudo su -c 'echo "
 [oracle]
 SigLevel = Optional TrustAll
-Server = http://linux.shikadi.net/arch/$repo/$arch
-" >> /etc/pacman.conf
+Server = http://linux.shikadi.net/arch/\$repo/\$arch
+" >> /etc/pacman.conf'
 
 sudo pacman -Sys oracle
 sudo pacman -S oracle-instantclient-sqlplus oracle-instantclient-sdk
