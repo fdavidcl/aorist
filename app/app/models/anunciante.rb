bf4f84c658481cb60d7e307daaa9a3df8bb745f6
JCEntrena <jentrena@correo.ugr.es>
2016-01-18 15:30:08 +0200

    Cambio en las asociaciones con audiencia.

--- app/app/models/anunciante.rb
+++ app/app/models/anunciante.rb
@@ -1,6 +1,6 @@
 class Anunciante < ActiveRecord::Base
   has_many :anuncios
   has_many :anunciante_contratos
-  has_many :audiences
+  has_and_belongs_to_many :audiences
   validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
 end
