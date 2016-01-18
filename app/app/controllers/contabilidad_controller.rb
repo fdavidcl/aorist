e035cb413d879a05930b0842a97f58a134ad28f4
luiscastro193 <luiscastro193@gmail.com>
2016-01-18 12:10:34 +0100

    Primera versón contabilidad

--- app/app/controllers/contabilidad_controller.rb
+++ app/app/controllers/contabilidad_controller.rb
@@ -1,12 +1,22 @@
 class ContabilidadController < ApplicationController
+  def suma_pagos
+    Pago.sum(:importe)
+  end
+  
+  def suma_cobros
+    Cobro.sum(:importe)
+  end
+  
   def index
   end
   
   def new
   end
   
-	def show
-		
-	end
+  def show
+    @balance_positivo = suma_cobros
+    @balance_negativo = suma_pagos
+    @balance_total = balance_positivo - balance_negativo
+  end
   
 end
