5d3dc12c9a734b345e281f9244de2b8571c0ba7a
David Charte <fdavidcl@outlook.com>
2016-01-19 11:58:18 +0100

    Vista vacía contabilidad

--- app/app/models/anunciante_contrato.rb
+++ app/app/models/anunciante_contrato.rb
@@ -1,6 +1,6 @@
 class AnuncianteContrato < ActiveRecord::Base
   belongs_to :anunciante
-  has_many :cobros
+  has_many :cobros, dependent: :destroy
   validates :importe, presence: true, numericality: {greather_than: 0}
   validates :fecha, :duracion, presence: true
   validates_date_of :duracion, after: :fecha
