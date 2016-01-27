50f0b9e41c3901c53ed7b11ae561916305ab416c
David Charte <fdavidcl@outlook.com>
2016-01-27 16:42:58 +0100

    Modifica tamaño viewport (para móvil)

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119161133) do
+ActiveRecord::Schema.define(version: 20160119161134) do
 
   create_table "anunciante_contratos", force: :cascade do |t|
     t.integer  "anunciante_id"
