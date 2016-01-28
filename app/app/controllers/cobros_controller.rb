02dc6a0cef34c3063fc6ca1f70a30583fdb678a6
JCEntrena <jentrena@correo.ugr.es>
2016-01-28 17:17:58 +0200

    Eliminando métodos innecesarios.

--- app/app/controllers/cobros_controller.rb
+++ app/app/controllers/cobros_controller.rb
@@ -2,31 +2,12 @@ class CobrosController < ApplicationController
 
   private
   def cobro_params
-  
-  end
-  
-  public
-  def new
-    @cobro = Cobro.new
-  end
-  
-  def create
-  
-  end
-  
-  def show
-  
-  end 
 
-  def index
-  
   end
-  
-  def destroy 
-    @cobro = Cobro.find_by id: params[:id]
-    @cobro.destroy
-    
-    redirect_to cobros_path       # Puede que haya que venir a través de anunciantes. 
-  end 
- 
+
+  public
+  def create
+
+  end
+
 end
--- app/app/controllers/pagos_controller.rb
+++ app/app/controllers/pagos_controller.rb
@@ -2,30 +2,12 @@ class PagosController < ApplicationController
 
   private
   def pago_params
-  
-  end
-  
-  public
-  def new
-    @pago = Pago.new
-  end
-  
-  def create
-    @pago = Pago.new pago_params
-  end
-  
-  def show
-  
-  end 
 
-  def index
-  
   end
-  
-  def destroy 
-    @pago = Pago.find_by id: params[:id]
-    @pago.destroy
+
+  public
+  def create
     
-    redirect_to pagos_path          # Puede que haya que venir a través de medios. 
-  end 
+  end
+
 end
