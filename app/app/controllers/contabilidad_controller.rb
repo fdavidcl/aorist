58c138e74462dfd78d78951ae18d682fa905c912
JCEntrena <jentrena@correo.ugr.es>
2016-01-28 15:32:08 +0200

    Separando en métodos.

--- app/app/controllers/contabilidad_controller.rb
+++ app/app/controllers/contabilidad_controller.rb
@@ -1,18 +1,32 @@
 class ContabilidadController < ApplicationController
-  def index
-  end
-  
-  def new
-  end
-  
-  def show
+helper_method :order_amount, :order_date, :group_contract, :group_owner
+
+  private
+  def order_amount
     @lista_cobros = Cobro.order(:importe)
     @lista_pagos = Pago.order(:importe)
-    @lista_por_anunciante_contrato = Cobro.group(:anunciante_contrato)
-    @lista_por_medio_contrato = Pago.group(:medio_contrato)
+  end
+
+  def order_date
+    @cobros_date = Cobro.order(:fecha)
+    @pagos_date = Pago.order(:fecha)
+  end
+
+  def group_contract
+    @cobros_contrato = Cobro.group(:anunciante_contrato)
+    @pagos_contrato = Pago.group(:medio_contrato)
+  end
+
+  def group_owner
+    @cobros_anunciante = Cobro.group(:anunciante)
+    @cobros_medio = Pago.group(:medio)
+  end
+
+  public
+  def show
     @balance_positivo = Cobro.sum(:importe)
     @balance_negativo = Pago.sum(:importe)
     @balance_total = @balance_positivo - @balance_negativo
   end
-  
+
 end
