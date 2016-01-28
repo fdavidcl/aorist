04a86786f06d4b9695a8fc40b107916da2d12483
JCEntrena <jentrena@correo.ugr.es>
2016-01-28 18:00:16 +0200

    Restricciones sobre la fecha.

--- app/app/models/pago.rb
+++ app/app/models/pago.rb
@@ -1,5 +1,6 @@
 class Pago < ActiveRecord::Base
   belongs_to :medio_contrato
   validates :importe, presence: true, numericality: {greater_than: 0}
-
+  validates :fecha, presence: true
+  validates_date_of :fecha, before: Proc.new { Time.now }
 end
