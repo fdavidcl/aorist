f20e115433774c46758b3fa4bb6ce915c8724e3a
JCEntrena <jentrena@correo.ugr.es>
2016-01-28 15:34:57 +0200

    Errata.

--- app/app/models/medio.rb
+++ app/app/models/medio.rb
@@ -1,7 +1,7 @@
 class Medio < ActiveRecord::Base
   has_many :medio_contratos, dependent: :destroy
   has_many :espacios, dependent: :destroy
-  has_many :pagos through :medio_contratos
+  has_many :pagos, through :medio_contratos
   has_and_belongs_to_many :audiences
   validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
 end
