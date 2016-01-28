4f2f389a4390e03d2644bd2750e575cc197d9e42
JCEntrena <jentrena@correo.ugr.es>
2016-01-28 15:36:29 +0200

    Errata (again) :squirrel:

--- app/app/models/anunciante.rb
+++ app/app/models/anunciante.rb
@@ -1,7 +1,7 @@
 class Anunciante < ActiveRecord::Base
   has_many :anuncios, dependent: :destroy
   has_many :anunciante_contratos, dependent: :destroy
-  has_many :cobros, through :anunciante_contratos
+  has_many :cobros, through: :anunciante_contratos
   has_and_belongs_to_many :audiences
   validates :nombre, :web, :fiscales, presence: true, length: {minimum: 1}
 end
