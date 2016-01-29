0c1ea3663999be0496c0c1e3505bd3ec8cc3ace3
David Charte <fdavidcl@outlook.com>
2016-01-29 20:15:10 +0100

    Eliminación de anuncios, espacios y relaciones entre ellos

--- app/app/models/anuncio.rb
+++ app/app/models/anuncio.rb
@@ -1,5 +1,5 @@
 class Anuncio < ActiveRecord::Base
   belongs_to :anunciante
-  has_many :anuncio_allocations
+  has_many :anuncio_allocations, dependent: :destroy
   has_many :espacios, through: :anuncio_allocations
 end
--- app/app/models/espacio.rb
+++ app/app/models/espacio.rb
@@ -1,5 +1,5 @@
 class Espacio < ActiveRecord::Base
   belongs_to :medio
-  has_one :anuncio_allocation
+  has_one :anuncio_allocation, dependent: :destroy
   has_one :anuncio, through: :anuncio_allocation
 end
