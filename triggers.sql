c8cce3f8ad3c69e2d96ece162bd70870f294a17b
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2016-02-04 16:24:02 +0100

    Triggers de cobros/pagos

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,4 @@
+-- 
 CREATE OR REPLACE TRIGGER enforce_space_compatibility
   AFTER
     UPDATE OF url
@@ -9,5 +10,51 @@ BEGIN
   WHERE anuncio_id = :NEW.id AND espacio_id IN (
     SELECT id FROM espacios WHERE enlace = 0
   );
+END;
+/
+
+-- Comprobar que al insertar un cobro asociado a un contrato, la suma de sus cobros no supere el importe definido.
+CREATE OR REPLACE TRIGGER check_cobros
+  BEFORE
+    INSERT
+  ON cobros
+  FOR EACH ROW
+DECLARE
+  suma      cobros.importe%TYPE := 0;
+  importe   cobros.importe%TYPE := 0;
+BEGIN
+  SELECT importe INTO importe
+  FROM anunciante_contratos
+  WHERE id = :NEW.anunciante_contrato_id;
+
+  SELECT SUM(importe) INTO suma
+  FROM cobros
+  WHERE anunciante_contrato_id = :NEW.anunciante_contrato_id;
+
+  suma := suma + :NEW.importe;
+END;
+/
+
+-- Comprobar que al insertar un pago asociado a un contrato, la suma de sus pagos no supere el importe definido.
+CREATE OR REPLACE TRIGGER check_pagos
+  BEFORE
+    INSERT
+  ON pagos
+  FOR EACH ROW
+DECLARE
+  suma      pagos.importe%TYPE := 0;
+  importe   pagos.importe%TYPE := 0;
+BEGIN
+  SELECT importe INTO importe
+  FROM medio_contratos
+  WHERE id = :NEW.medio_contrato_id;
+
+  SELECT SUM(importe) INTO suma
+  FROM pagos
+  WHERE medio_contrato_id = :NEW.medio_contrato_id;
+
+  suma := suma + :NEW.importe;
+
+  IF suma > importe;
 END;
 /
